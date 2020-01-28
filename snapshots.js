741b7b0943d74454732dab27c81c99673cbec5aa
Paul Butler III <paul@heysparkbox.com>
2020-01-28 12:26:36 -0700

    updated test

--- snapshots.js
+++ snapshots.js
@@ -1,7 +1,7 @@
 const PercyScript = require('@percy/script');
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:3001/');
+  await page.goto('http://localhost:3000/');
   // ensure the page has loaded before capturing a snapshot
   // await page.waitFor('.a-selector-on-page');
   await percySnapshot('homepage');
